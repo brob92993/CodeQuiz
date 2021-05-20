52c87f24f0de34f4611313ab7bb8777023b0570f
Brian Robinson <brob92993@gmail.com>
2021-05-20 12:36:44 -0400

    added functions to show the questions when the timer starts, and started the compare function to compare choices with the correct answer

--- questions.js
+++ questions.js
@@ -3,14 +3,15 @@ var currentTime = document.querySelector("#currentTime");
 var divQuestions = document.querySelector("#questions");
 var choices = document.querySelector("#choices");
 var timer = document.querySelector("#start");
+
 var ulCreate = document.createElement("ul");
 
-var secondsLeft = 100;
+var timeLeft = 100;
 var penalty = 10;
 var holdInterval = 0;
 
 var score = 0;
-var questionsIndex = 0;
+var questionIndex = 0;
 
 // Object variable for the questions with the name, options, and answer
 var questions = [
@@ -41,21 +42,55 @@ var questions = [
     },
 
 ];
-
+// function to show the timer; notifies that time is up when there is no time left
 timer.addEventListener("click", function() {
     if (holdInterval === 0) {
         holdInterval = setInterval (function() {
-            secondsLeft--;
-            currentTime.textContent = "Time: " + secondsLeft;
+            timeLeft--;
+            currentTime.textContent = "Time: " + timeLeft;
 
-    if (secondsLeft <= 0) {
+    if (timeLeft <= 0) {
         clearInterval();
         allDone();
         currentTime.textContent = "TIme's up!"
         }
     }, 1000);
 }
-render(questionsIndex);
+render(questionIndex);
 
 });
+
+function render(questionIndex) {
+    // Clears existing content inside the questions div and the created list 
+    divQuestions.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    // For loops to loop through all info in array
+    for (var i = 0; i < questions.length; i++) {
+        // for each question, only show the question itself first, followed by the choices to pick from
+        var userQuestion = questions[questionIndex].title;
+        var userOptions = questions[questionIndex].options;
+        divQuestions.textContent = userQuestion; // the content of the questions div will be the question title
+    }
+
+    // for each function for each question
+    userOptions.forEach(function (newItem) {
+        var liCreate = document.createElement("li");
+        liCreate.textContent = newItem;
+        questionsDiv.appendChild(ulCreate); //attach the unordered list below the questions div
+        ulCreate.appendChild(liCreate); 
+        liCreate.addEventListener("click", (compare)); // when an list option is clicked, compare that option to the answer
+    })
+}
+
+// this will compare the clicked li from the function above, with the answers
+function compare(event) {
+   var selection = event.target;
+   
+   if (selection.matches("li")) {
+
+    var divCreate = document.createElement ("div");
+    divCreate.setAttribute("id", "divCreate");
+   }
+}
 
