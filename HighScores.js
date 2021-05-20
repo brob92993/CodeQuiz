ee272d2ffccd234b9a11ae37d6971ff258eea6c1
Brian Robinson <brob92993@gmail.com>
2021-05-20 10:56:14 -0400

    updated the highscores script with the function to clear scores and go back to the original page

--- HighScores.js
+++ HighScores.js
@@ -1,3 +1,24 @@
 var highScore = document.querySelector("#highscore");
 var reset = document.querySelector("#reset");
 var back = document.querySelector("#back");
+
+
+// listen for a click to clear/reset the scores
+reset.addEventListener("click", function() {
+ localStorage.clear();
+ location.reload();   
+});
+
+var allScores = localStorage.getItem("allScores");
+allScores - JSON.parse(allScores);
+
+if (allScores !== null) {
+   for (var i = 0; i < allScores.length; i++) {
+       createLi.textcontent = allScores [i].initials + " " + allScores[i].score;
+       highScore.appendChild(createLi);
+   } 
+}
+
+back.addEventListener("click", function() {
+    window.location.replace("./index.html");
+});
